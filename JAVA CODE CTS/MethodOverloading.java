f52e224e1cc09e26f8967ea91f1b973236416aa6
Yaminee  Lakshya.S <yamineelakshya5@gmail.com>
2026-05-30 15:57:14 +0530

    Update MethodOverloading.java

--- JAVA CODE CTS/MethodOverloading.java
+++ JAVA CODE CTS/MethodOverloading.java
@@ -14,16 +14,16 @@ public class MethodOverloading {
         System.out.print("Enter two integers: ");
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.println("Sum of two integers     : " + add(a, b));
+        System.out.println("Sum of two integers: " + add(a, b));
         System.out.print("Enter two doubles: ");
         double x = sc.nextDouble();
         double y = sc.nextDouble();
-        System.out.println("Sum of two doubles      : " + add(x, y));
+        System.out.println("Sum of two doubles: " + add(x, y));
         System.out.print("Enter three integers: ");
         int p = sc.nextInt();
         int q = sc.nextInt();
         int r = sc.nextInt();
-        System.out.println("Sum of three integers   : " + add(p, q, r));
+        System.out.println("Sum of three integers: " + add(p, q, r));
         sc.close();
     }
 }
