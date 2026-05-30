62b3d873b50c3dc9a2a10a808c8d8892f2c62eec
Yaminee  Lakshya.S <yamineelakshya5@gmail.com>
2026-05-30 15:56:38 +0530

    Update Interface.java

--- JAVA CODE CTS/Interface.java
+++ JAVA CODE CTS/Interface.java
@@ -2,15 +2,13 @@ interface Playable {
     void play();
 }
 class Guitar implements Playable {
-    @Override
     public void play() {
-        System.out.println("Playing Guitar ");
+        System.out.println("Playing Guitar");
     }
 }
 class Piano implements Playable {
-    @Override
     public void play() {
-        System.out.println("Playing Piano ");
+        System.out.println("Playing Piano");
     }
 }
 public class Interface {
